521ff417ef00649ac9e7780e7b60afa7e847819b
Abhijeet <abhijeetdhopare28@gmail.com>
2024-03-04 19:21:23 +0530

    change

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("./db/connection");
 const calculatorRouter = require("./routers/calculatorData");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(calculatorRouter);
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/calculator")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connection success......");
   })
